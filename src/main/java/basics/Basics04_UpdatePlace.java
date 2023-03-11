4e68f5def5aae4332282e037980258fc26945610
Hasan YUCEL <hsnnycll@gmail.com>
2023-03-11 10:30:34 +0300

    add new file

---
+++ src/main/java/basics/Basics04_UpdatePlace.java
@@ -0,0 +1,38 @@
+package basics;
+
+import files.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basics04_UpdatePlace {
+    public static void main(String[] args) {
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(Payload.addPlace())
+                .when().post("maps/api/place/add/json")
+                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+
+        //System.out.println(response);
+
+        JsonPath js = new JsonPath(response); //for parsing Json
+        String placeId = js.getString("place_id");
+
+        //System.out.println("place_id: " + placeId);
+
+        //Update Place
+
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"place_id\":\"" + placeId + "\",\n" +
+                        "\"address\":\"70 winter walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+    }
+}
