709402755530a5a2b8c0d407f99f7b71b3bc17fd
Hasan YUCEL <hsnnycll@gmail.com>
2023-03-16 20:38:58 +0300

    add new file

---
+++ src/main/java/oAuth/OAuthTest.java
@@ -0,0 +1,29 @@
+package oAuth;
+
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.restassured.RestAssured.given;
+
+public class OAuthTest {
+    public static void main(String[] args) {
+
+        String accessTokenResponse = given().queryParam("code", "")
+                .queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+                .queryParam("grant_type", "authorization_code")
+                .when().log().all()
+                .post("https://www.googleapis.com/oauth2/v4/token").asString();
+
+        JsonPath js = new JsonPath(accessTokenResponse);
+        String accessToken = js.getString("access_token");
+
+        String response = given().queryParam("access_token", accessToken)
+                .when().log().all()
+                .get("https://rahulshettyacademy.com/getCourse.php").asString();
+        System.out.println(response);
+    }
+}
