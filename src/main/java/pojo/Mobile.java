e093ded0466fb0206a1d571fa0063e8cac5159aa
Hasan YUCEL <hsnnycll@gmail.com>
2023-03-19 10:33:24 +0300

    add new file

---
+++ src/main/java/pojo/Mobile.java
@@ -0,0 +1,24 @@
+package pojo;
+
+public class Mobile {
+
+    private String courseTitle;
+    private String price;
+
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
